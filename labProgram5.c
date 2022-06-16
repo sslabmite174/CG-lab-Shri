f61ba0135d476bb091d5eba62ef8435c4d03d66d
Shrinidhi A <shrinidhisringeri@gmail.com>
2022-06-16 11:41:46 +0530

    Update labProgram5.c

--- labProgram5.c
+++ labProgram5.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 #include<GL/glut.h>
-float v[4][3] = {
+typedef float point[3];
+point v[] = 	{
 		{0.0, 0.0, 1.0},
 		{0.0, 1.0, 0.0},
 		{-1.0, -0.5, 0.0},
@@ -14,15 +15,15 @@ float colors[4][3] = 	{
 			};
 int n;
 
-void triangle(float *va, float *vb, float *vc){
+void triangle(point a, point b, point c){
 	glBegin(GL_TRIANGLES);
-	glVertex3fv(va);
-	glVertex3fv(vb);
-	glVertex3fv(vc);
+	glVertex3fv(a);
+	glVertex3fv(b);
+	glVertex3fv(c);
 	glEnd();
 }
 
-void tetra(float *a, float *b, float *c, float *d){
+/*void tetra(float *a, float *b, float *c, float *d){
 	glColor3fv(colors[0]);
 	triangle(a,b,c); 
 	glColor3fv(colors[1]);
@@ -32,36 +33,40 @@ void tetra(float *a, float *b, float *c, float *d){
 	glColor3fv(colors[3]);
 	triangle(b,d,c);
 }
-
-void divide_tetra(float *a, float *b, float *c, float *d, int m){
-	float mid[6][3];
+*/
+void divide_tetra(point a, point b, point c, int m){
+	point v1,v2,v3;
 	int j;
 	if(m>0){
-		for(j=0;j<3;j++)
-			mid[0][j] = (a[j]+b[j])/2;
-		for(j=0;j<3;j++)
-			mid[1][j] = (a[j]+c[j])/2;
-		for(j=0;j<3;j++)
-			mid[2][j] = (a[j]+d[j])/2;
-		for(j=0;j<3;j++)
-			mid[3][j] = (b[j]+c[j])/2;
-		for(j=0;j<3;j++)
-			mid[4][j] = (c[j]+d[j])/2;
-		for(j=0;j<3;j++)
-			mid[5][j] = (b[j]+d[j])/2;
-		divide_tetra(a, mid[0], mid[1], mid[2], m-1);
-		divide_tetra(mid[0], b, mid[3], mid[5], m-1);
-		divide_tetra(mid[1], mid[3],c , mid[4], m-1);
-		divide_tetra(mid[2], mid[5], mid[4],d , m-1);
+		for(j=0;j<3;j++){
+			v1[j] = (a[j]+b[j])/2;
+			v2[j] = (a[j]+c[j])/2;
+			v3[j] = (b[j]+c[j])/2;
+			
+		}
+		divide_tetra(a,v1,v2,m-1);
+		divide_tetra(c,v2,v3,m-1);
+		divide_tetra(b,v3,v1,m-1);
 	}
 	else
-		tetra(a, b, c, d);
+		triangle(a,b,c);
 }
 
-void display(){
+void tetrahedron(int m){
+	glColor3f(1.0,0.0,0.0);
+	divide_tetra(v[0],v[1],v[2],m);
+	glColor3f(0.0,1.0,0.0);
+	divide_tetra(v[3],v[2],v[1],m);
+	glColor3f(0.0,0.0,1.0);
+	divide_tetra(v[0],v[3],v[1],m);
+	glColor3f(0.0,0.0,0.0);
+	divide_tetra(v[0],v[2],v[3],m);
+}
+
+void display(void){
 	glClearColor(1.0, 1.0, 1.0, 1.0);
 	glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
-	divide_tetra(v[0], v[1], v[2], v[3], n);
+        tetrahedron(n);
 	glFlush();
 }
 
@@ -91,4 +96,5 @@ int main(int argc, char *argv[]){
 	return 0;
 }
 
+		
 		
